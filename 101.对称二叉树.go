ec1bacd06fcdc4299a9c339f975730d1acc29e70
FarmerChillax <farmer-chong@qq.com>
2023-09-29 00:20:21 +0800

    chore: add package

--- 101.对称二叉树.go
+++ 101.对称二叉树.go
@@ -14,36 +14,36 @@
  * Testcase Example:  '[1,2,2,3,4,4,3]'
  *
  * 给你一个二叉树的根节点 root ， 检查它是否轴对称。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：root = [1,2,2,3,4,4,3]
  * 输出：true
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：root = [1,2,2,null,3,null,3]
  * 输出：false
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 树中节点数目在范围 [1, 1000] 内
  * -100 <= Node.val <= 100
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 进阶：你可以运用递归和迭代两种方法解决这个问题吗？
- * 
+ *
  */
 
 // @lc code=start
@@ -55,42 +55,36 @@
  *     Right *TreeNode
  * }
  */
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
- func isSymmetric(root *TreeNode) bool {
-    if root.Left != nil || root.Right != nil {
-        return check(root.Left, root.Right)
-    }
-    return true
+package algorithm
+
+func isSymmetric(root *TreeNode) bool {
+	if root.Left != nil || root.Right != nil {
+		return check(root.Left, root.Right)
+	}
+	return true
 }
 
 func check(left, right *TreeNode) bool {
-    if left == nil && right == nil {
-        return true
-    }
+	if left == nil && right == nil {
+		return true
+	}
 
-    if left == nil || right == nil {
-        return false
-    }
+	if left == nil || right == nil {
+		return false
+	}
 
-    if left.Val == right.Val {
-        // 当前节点匹配成功（对称）
-        if !check(left.Left, right.Right) {
-            return false
-        }
-        if !check(left.Right, right.Left) {
-            return false
-        }
-        return true
-    }
+	if left.Val == right.Val {
+		// 当前节点匹配成功（对称）
+		if !check(left.Left, right.Right) {
+			return false
+		}
+		if !check(left.Right, right.Left) {
+			return false
+		}
+		return true
+	}
 
-    return false
+	return false
 }
-// @lc code=end
 
+// @lc code=end
