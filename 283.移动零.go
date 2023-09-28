e62560ce5900c666066d9bbf73d8280590d6d1bd
FarmerChillax <farmer-chong@qq.com>
2023-09-29 00:18:22 +0800

    add

---
+++ 283.移动零.go
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=golang
+ *
+ * [283] 移动零
+ *
+ * https://leetcode.cn/problems/move-zeroes/description/
+ *
+ * algorithms
+ * Easy (63.67%)
+ * Likes:    2172
+ * Dislikes: 0
+ * Total Accepted:    1.1M
+ * Total Submissions: 1.8M
+ * Testcase Example:  '[0,1,0,3,12]'
+ *
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ *
+ * 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: nums = [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: nums = [0]
+ * 输出: [0]
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ *
+ * 1 <= nums.length <= 10^4
+ * -2^31 <= nums[i] <= 2^31 - 1
+ *
+ *
+ *
+ *
+ * 进阶：你能尽量减少完成的操作次数吗？
+ *
+ */
+
+package algorithm
+
+// @lc code=start
+func moveZeroes(nums []int) {
+	var left int
+	for right := 0; right < len(nums); right++ {
+		if nums[right] != 0 {
+			nums[left], nums[right] = nums[right], nums[left]
+			left++
+		}
+	}
+}
+
+// @lc code=end
---
+++ 49.字母异位词分组.go
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode.cn id=49 lang=golang
+ *
+ * [49] 字母异位词分组
+ *
+ * https://leetcode.cn/problems/group-anagrams/description/
+ *
+ * algorithms
+ * Medium (67.81%)
+ * Likes:    1635
+ * Dislikes: 0
+ * Total Accepted:    521.7K
+ * Total Submissions: 769.9K
+ * Testcase Example:  '["eat","tea","tan","ate","nat","bat"]'
+ *
+ * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+ *
+ * 字母异位词 是由重新排列源单词的所有字母得到的一个新单词。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: strs = [""]
+ * 输出: [[""]]
+ *
+ *
+ * 示例 3:
+ *
+ *
+ * 输入: strs = ["a"]
+ * 输出: [["a"]]
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= strs.length <= 10^4
+ * 0 <= strs[i].length <= 100
+ * strs[i] 仅包含小写字母
+ *
+ *
+ */
+
+package algorithm
+
+// @lc code=start
+func groupAnagrams(strs []string) [][]string {
+	mp := map[[26]int][]string{}
+	for _, str := range strs {
+		cnt := [26]int{}
+		for _, b := range str {
+			cnt[b-'a']++
+		}
+		mp[cnt] = append(mp[cnt], str)
+	}
+	ans := make([][]string, 0, len(mp))
+	for _, v := range mp {
+		ans = append(ans, v)
+	}
+	return ans
+}
+
+// @lc code=end
