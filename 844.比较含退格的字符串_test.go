39f3fe30f8ffa60d5cdd87e3523890821f1501fc
FarmerChillax <farmer-chong@qq.com>
2023-10-16 21:29:51 +0800

    feat: add 844

---
+++ 844.比较含退格的字符串_test.go
@@ -0,0 +1,32 @@
+package algorithm
+
+import "testing"
+
+func Test_backspaceCompare(t *testing.T) {
+	type args struct {
+		s string
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case-1",
+			args: args{
+				s: "y#fo##f",
+				t: "y#f#o##f",
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := backspaceCompare(tt.args.s, tt.args.t); got != tt.want {
+				t.Errorf("backspaceCompare() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
