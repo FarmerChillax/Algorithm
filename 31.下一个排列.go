832b7aa220d5f0087de914f270fb64e00183d634
FarmerChillax <farmer-chong@qq.com>
2023-11-06 14:33:29 +0800

    feat: add 31 with bytesdance

---
+++ 31.下一个排列.go
@@ -0,0 +1,95 @@
+/*
+ * @lc app=leetcode.cn id=31 lang=golang
+ *
+ * [31] 下一个排列
+ *
+ * https://leetcode.cn/problems/next-permutation/description/
+ *
+ * algorithms
+ * Medium (38.60%)
+ * Likes:    2363
+ * Dislikes: 0
+ * Total Accepted:    467.4K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 整数数组的一个 排列  就是将其所有成员以序列或线性顺序排列。
+ *
+ *
+ * 例如，arr = [1,2,3] ，以下这些都可以视作 arr 的排列：[1,2,3]、[1,3,2]、[3,1,2]、[2,3,1] 。
+ *
+ *
+ * 整数数组的 下一个排列 是指其整数的下一个字典序更大的排列。更正式地，如果数组的所有排列根据其字典顺序从小到大排列在一个容器中，那么数组的 下一个排列
+ * 就是在这个有序容器中排在它后面的那个排列。如果不存在下一个更大的排列，那么这个数组必须重排为字典序最小的排列（即，其元素按升序排列）。
+ *
+ *
+ * 例如，arr = [1,2,3] 的下一个排列是 [1,3,2] 。
+ * 类似地，arr = [2,3,1] 的下一个排列是 [3,1,2] 。
+ * 而 arr = [3,2,1] 的下一个排列是 [1,2,3] ，因为 [3,2,1] 不存在一个字典序更大的排列。
+ *
+ *
+ * 给你一个整数数组 nums ，找出 nums 的下一个排列。
+ *
+ * 必须 原地 修改，只允许使用额外常数空间。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [1,2,3]
+ * 输出：[1,3,2]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [3,2,1]
+ * 输出：[1,2,3]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：nums = [1,1,5]
+ * 输出：[1,5,1]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 100
+ *
+ *
+ */
+package algorithm
+
+// @lc code=start
+func nextPermutation(nums []int) {
+	if len(nums) == 1 {
+		return
+	}
+	var index int
+	for index = len(nums) - 2; index >= 0; index-- {
+		if nums[index+1] > nums[index] {
+			break
+		}
+	}
+
+	if index >= 0 {
+		i := len(nums) - 1
+		for i > index && nums[index] >= nums[i] {
+			i--
+		}
+		nums[index], nums[i] = nums[i], nums[index]
+	}
+
+	for index, j := index+1, len(nums)-1; index < j; index, j = index+1, j-1 {
+		nums[index], nums[j] = nums[j], nums[index]
+	}
+}
+
+// @lc code=end
---
+++ 31.下一个排列_test.go
@@ -0,0 +1,129 @@
+/*
+ * @lc app=leetcode.cn id=31 lang=golang
+ *
+ * [31] 下一个排列
+ *
+ * https://leetcode.cn/problems/next-permutation/description/
+ *
+ * algorithms
+ * Medium (38.60%)
+ * Likes:    2363
+ * Dislikes: 0
+ * Total Accepted:    467.4K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 整数数组的一个 排列  就是将其所有成员以序列或线性顺序排列。
+ *
+ *
+ * 例如，arr = [1,2,3] ，以下这些都可以视作 arr 的排列：[1,2,3]、[1,3,2]、[3,1,2]、[2,3,1] 。
+ *
+ *
+ * 整数数组的 下一个排列 是指其整数的下一个字典序更大的排列。更正式地，如果数组的所有排列根据其字典顺序从小到大排列在一个容器中，那么数组的 下一个排列
+ * 就是在这个有序容器中排在它后面的那个排列。如果不存在下一个更大的排列，那么这个数组必须重排为字典序最小的排列（即，其元素按升序排列）。
+ *
+ *
+ * 例如，arr = [1,2,3] 的下一个排列是 [1,3,2] 。
+ * 类似地，arr = [2,3,1] 的下一个排列是 [3,1,2] 。
+ * 而 arr = [3,2,1] 的下一个排列是 [1,2,3] ，因为 [3,2,1] 不存在一个字典序更大的排列。
+ *
+ *
+ * 给你一个整数数组 nums ，找出 nums 的下一个排列。
+ *
+ * 必须 原地 修改，只允许使用额外常数空间。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [1,2,3]
+ * 输出：[1,3,2]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [3,2,1]
+ * 输出：[1,2,3]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：nums = [1,1,5]
+ * 输出：[1,5,1]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 100
+ *
+ *
+ */
+package algorithm
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_nextPermutation(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case-1",
+			args: args{
+				nums: []int{1, 2, 3},
+			},
+			want: []int{1, 3, 2},
+		},
+		{
+			name: "case-2",
+			args: args{
+				nums: []int{3, 2, 1},
+			},
+			want: []int{1, 2, 3},
+		},
+		{
+			name: "case-3",
+			args: args{
+				nums: []int{1, 1, 5},
+			},
+			want: []int{1, 5, 1},
+		},
+		{
+			name: "case-4",
+			args: args{
+				nums: []int{1, 3, 2},
+			},
+			want: []int{2, 1, 3},
+		},
+		{
+			name: "case-5",
+			args: args{
+				nums: []int{2, 3, 1},
+			},
+			want: []int{3, 1, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			nextPermutation(tt.args.nums)
+			if !reflect.DeepEqual(tt.args.nums, tt.want) {
+				t.Errorf("nextPermutation() = %v, want %v", tt.args.nums, tt.want)
+			}
+		})
+	}
+}
