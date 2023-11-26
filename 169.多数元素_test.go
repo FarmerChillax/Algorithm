3333ba98e222bf022627e6caf36c2ea570d1cd92
FarmerChillax <farmer-chong@qq.com>
2023-11-26 17:50:33 +0800

    feat: add 169

---
+++ 169.多数元素_test.go
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode.cn id=169 lang=golang
+ *
+ * [169] 多数元素
+ *
+ * https://leetcode.cn/problems/majority-element/description/
+ *
+ * algorithms
+ * Easy (66.46%)
+ * Likes:    2045
+ * Dislikes: 0
+ * Total Accepted:    809.3K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[3,2,3]'
+ *
+ * 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+ *
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [3,2,3]
+ * 输出：3
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [2,2,1,1,1,2,2]
+ * 输出：2
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * n == nums.length
+ * 1 <= n <= 5 * 10^4
+ * -10^9 <= nums[i] <= 10^9
+ *
+ *
+ *
+ *
+ * 进阶：尝试设计时间复杂度为 O(n)、空间复杂度为 O(1) 的算法解决此问题。
+ *
+ */
+package algorithm
+
+import "testing"
+
+func Test_majorityElement(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case-1",
+			args: args{
+				nums: []int{3, 2, 3},
+			},
+			want: 3,
+		},
+		{
+			name: "case-2",
+			args: args{
+				nums: []int{2, 2, 1, 1, 1, 2, 2},
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := majorityElement(tt.args.nums); got != tt.want {
+				t.Errorf("majorityElement() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
