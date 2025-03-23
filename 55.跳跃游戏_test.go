fb889fbd1c5bb9b719ed9f7ed6a2c258d39ba207
FarmerChillax <farmerchillax@outlook.com>
2025-03-23 14:30:45 +0800

    feat add 55

---
+++ 55.跳跃游戏_test.go
@@ -0,0 +1,98 @@
+/*
+ * @lc app=leetcode.cn id=55 lang=golang
+ *
+ * [55] 跳跃游戏
+ *
+ * https://leetcode.cn/problems/jump-game/description/
+ *
+ * algorithms
+ * Medium (43.33%)
+ * Likes:    2995
+ * Dislikes: 0
+ * Total Accepted:    1.2M
+ * Total Submissions: 2.7M
+ * Testcase Example:  '[2,3,1,1,4]'
+ *
+ * 给你一个非负整数数组 nums ，你最初位于数组的 第一个下标 。数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ *
+ * 判断你是否能够到达最后一个下标，如果可以，返回 true ；否则，返回 false 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [2,3,1,1,4]
+ * 输出：true
+ * 解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [3,2,1,0,4]
+ * 输出：false
+ * 解释：无论怎样，总会到达下标为 3 的位置。但该下标的最大跳跃长度是 0 ， 所以永远不可能到达最后一个下标。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 10^4
+ * 0 <= nums[i] <= 10^5
+ *
+ *
+ */
+package algorithm
+
+import "testing"
+
+func Test_canJump(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case-1",
+			args: args{
+				nums: []int{2, 3, 1, 1, 4},
+			},
+			want: true,
+		},
+		{
+			name: "case-2",
+			args: args{
+				nums: []int{3, 2, 1, 0, 4},
+			},
+			want: false,
+		},
+		{
+			name: "case-3",
+			args: args{
+				nums: []int{0},
+			},
+			want: true,
+		},
+		{
+			name: "case-4",
+			args: args{
+				nums: []int{2, 0, 0},
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canJump(tt.args.nums); got != tt.want {
+				t.Errorf("canJump() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
